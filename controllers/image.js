1d76b855388e974f0ac32ce78716712f67dbb16e
sohostory <sohostory@gmail.com>
2023-05-06 14:46:45 -0500

    added controler folder and fixed bugs

---
+++ controllers/image.js
@@ -0,0 +1,55 @@
+const returnClarifaiRequestOptions = (imageUrl) => {
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: "sohostory",
+      app_id: "face-detection-app",
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: imageUrl,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Key " + "6437891f3e984e09ac3bd1285cfd8eec",
+    },
+    body: raw,
+  };
+  return requestOptions;
+};
+
+const handleApiCall = (req, res) => {
+  fetch(
+    `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
+    returnClarifaiRequestOptions(req.body.input)
+  )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("unable to work with api"));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+};
+
+module.exports = {
+  handleImage,
+  handleApiCall,
+};
